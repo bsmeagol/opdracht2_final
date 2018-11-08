65eecc5c70993ab9bcb6d24753d14dcff3a2c468
benjamin <bsmeagol@hotmail.com>
2018-11-08 01:39:25 +0100

    final

--- cgi_post_to_json.c
+++ cgi_post_to_json.c
@@ -1,24 +1,38 @@
-#include <stdio.h>				// You need this #include to define the printf function
+#include <time.h>
+#include <wiringPi.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <stdio.h>				
 #include <stdlib.h>
+#include <string.h>
 
-
-int test = 5;
 
 void main(int argc, char *argv[])
 {
-    printf("Content-type: text/html\n\n");
-
-	
-
-
-    printf("<html>");
-    printf("<head>");
-    printf("<title>...</title>");
-    printf("</head>");
-    printf("<body>");
-    printf("<p>wrote to json !</p>");
     
-    printf("</body>");
-    printf("</html>");
-    return;
+printf("Content-type: text/html;charset=utf-8\n\n");
+
+
+char Buffer[512]={0};
+fread( Buffer, 50, 1, stdin );
+
+if(strlen(Buffer)!=0)
+{
+printf("post received!\r\n");
+//write to json
+
+time_t t = time(NULL);
+struct tm tm = *localtime(&t);
+printf("now: %d-%d-%d %d:%d:%d\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+
+
+}
+else
+{
+printf("no post received...\r\n");
+}
+
+
+
+
 }
